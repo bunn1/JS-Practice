c3c6cb22014738baf5a8f472fb564f6ed74a792a
bunn1 <nicjohbun@gmail.com>
2022-03-01 21:13:24 +0100

    jstriangle

--- ovningar.js
+++ ovningar.js
@@ -6,3 +6,19 @@ for (let i=0; i < 8; i++) {
     x='#' + x;
     console.log(x);
 }
+
+// Triangle Ex 2 s 37--------------------
+
+counter = "#"
+
+for (let columns = 0; columns <= 8; columns++) {
+   counter += "#"
+    console.log (counter)
+}
+
+// Triangle Ex 3 s 37
+
+for(let line= "#"; line.length < 8; line += "#")
+console.log(line)
+
+// FizzBuzz s 38-----------------------------
